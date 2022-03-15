1d0c9f2961d8599f5d337c82ddaa56b64ae49817
Vitor Dutra <jo.vitor.d@gmail.com>
2022-03-15 14:57:38 -0300

    Finish march 15 lecture

--- src/App.js
+++ src/App.js
@@ -1,11 +1,28 @@
+import { useState } from 'react';
 import './styles.css';
 import Button from './components/Button';
 
+const task = {
+  id: '1',
+  nome: 'Fazer Café',
+  status: false,
+};
+
 function App() {
+  const [textInput, setTextInput] = useState('');
+  const [tasks, setTasks] = useState([
+    { id: '1', nome: 'Fazer Café', status: false },
+    { id: '2', nome: 'Estudar React', status: true },
+  ]);
+
+  function handleTextInput(event) {
+    setTextInput(event.target.value);
+  }
   return (
     <>
       <header className='header'>
-        <input className='input' />
+        <input className='input' onChange={event => handleTextInput(event)} />
+        {textInput}
         <div className='buttons-container'>
           <Button text='Adicionar' />
           <Button text='Limpar' />
